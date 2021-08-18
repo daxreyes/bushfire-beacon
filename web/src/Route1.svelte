57ae951acae06c901c98a8c13add8600f10276b3
Dax Reyes <dax.reyes@woohoow.com>
2021-08-19 05:12:06 +1000

    Add radio button examples

--- web/src/Route1.svelte
+++ web/src/Route1.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { Button, Dialog, TextField, Tabs, Tab, Image } from "smelte";
+    import { RadioButtonGroup, Tabs, Tab, Image } from "smelte";
     let showDialog2 = false;
     let loading = false;
 
@@ -9,17 +9,19 @@
     class="relative lg:max-w-3xl mx-auto mb-10 mt-24 md:max-w-md md:px-3"
 >
 <h1>Route1!</h1>
-<Dialog persistent bind:value={showDialog2}>
-    <h5 slot="title">Do you think you can close me by clicking outside?</h5>
-    <div class="text-gray-700 dark:text-gray-100">Doubt it.</div>
-    <div slot="actions">
-      <Button text on:click={() => (showDialog2 = false)}>Yes</Button>
-      <Button text on:click={() => (showDialog2 = false)}>No</Button>
-    </div>
-  </Dialog>
-  <TextField label="Label" placeholder="Input here" prepend="whatshot" outlined />
+<!-- check rollup.config.js for available color -->
+<RadioButtonGroup
+  name="Colored test"
+  color="blue"
+  disabled={false}
+  classes='inline'
+  items={[{ value: 1, label: 'One' }, { value: 2, label: 'Two' }]} />
 
-<Button color="alert" on:click={() => (showDialog2 = true)}>Show modal</Button>
+  <RadioButtonGroup
+  name="Colored test"
+  color="secondary"
+  disabled={false}
+  items={[{ value: 1, label: 'One' }, { value: 2, label: 'Two' }]} />
 
 <Tabs
     selected="1"
